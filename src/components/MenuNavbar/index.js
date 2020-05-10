8baa40a97fd4e8ee9d328786687b2c57f49ef845
Sayon Sivakumaran <sivakumaransayon@gmail.com>
2020-05-10 13:36:14 -0400

    add menu navbar

--- src/components/MenuNavbar/index.js
+++ src/components/MenuNavbar/index.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import { Nav, Navbar } from 'react-bootstrap';
 import './styles.css';
 
-// TODO change nav item list so its horizontal and appropriately styled
 export default () => (
-  <Navbar>
-    <Nav className='menu-nav'>
-      <Nav.Item className='menu-nav-item'><Nav.Link>Home</Nav.Link></Nav.Item>
-      <Nav.Item className='menu-nav-item'><Nav.Link>Home</Nav.Link></Nav.Item>
-      <Nav.Item className='menu-nav-item'><Nav.Link>Home</Nav.Link></Nav.Item>
-    </Nav>
-  </Navbar>
+  <ul className='menu-nav'>
+    <li><a>About</a></li>
+    <li><a>Projects</a></li>
+    <li><a>Resume</a></li>
+  </ul>
 );
