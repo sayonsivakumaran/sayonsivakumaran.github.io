8e7a71336f4455e8c2fabddb08e10cf2a841cff9
Sayon Sivakumaran <sivakumaransayon@gmail.com>
2020-05-10 21:49:05 -0400

    fix link bug

--- src/components/SocialMediaNavbar/index.js
+++ src/components/SocialMediaNavbar/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './styles.css';
+import '@fortawesome/fontawesome-free/css/fontawesome.min.css'
 
 export default () => (
   <ul className='social-media-nav'>
-    <li><a href='https://www.linkedin.com/in/sayonsivakumaran/'><i class="fab fa-linkedin-in fa-lg"></i></a></li>
-    <li><a href="mailto:sayon.sivakumaran@gmail.com"><i class="far fa-envelope fa-lg"></i></a></li>
-    <li><a href="https://www.github.com/sayonsivakumaran"><i class="fab fa-github fa-lg"></i></a></li>
-    <li><a href="https://www.facebook.com/sayonsivakumaran.18"><i class="fab fa-facebook-f fa-lg"></i></a></li>
+    <li><a href='https://www.linkedin.com/in/sayonsivakumaran/'><i className="fa fa-linkedin fa-lg"></i></a></li>
+    <li><a href="mailto:sayon.sivakumaran@gmail.com"><i className="fa fa-envelope fa-lg"></i></a></li>
+    <li><a href="https://www.github.com/sayonsivakumaran"><i className="fa fa-github fa-lg"></i></a></li>
+    <li><a href="https://www.facebook.com/sayonsivakumaran.18"><i className="fa fa-facebook fa-lg"></i></a></li>
   </ul>
 );
