2d9761cb4b8dc09127bfa4f833bbd195044a60aa
Sayon Sivakumaran <sivakumaransayon@gmail.com>
2020-09-15 17:48:57 -0400

    Complete course page

--- src/routes/About/index.js
+++ src/routes/About/index.js
@@ -17,7 +17,7 @@ export default () => (
           I'm <b>Sayon Sivakumaran</b>, a third year Computer Science and Statistics student at the <b>University of Toronto</b>,
           where I am working as a Teaching Assistant for the Introductory Probability course STAB52.
           I am currently working as a <b>Data Engineer</b>, and have prior experience working as a <b>Full Stack Engineer</b>.
-          When I'm not working or studying, I spend most of my time surfing the internet, and watching or reading about movies and television.
+          When I'm not working or studying, I spend much of my time surfing the internet, and watching or reading about movies and television.
           Thanks for visiting my site! Feel free to check out some of the projects I've worked on on <a href='https://github.com/sayonsivakumaran'>GitHub</a>, and don't hesistate to reach out!
         </p>
       </div>
--- src/routes/Teaching/routes/STAB52F20/index.js
+++ src/routes/Teaching/routes/STAB52F20/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Layout, VerticalNavbar } from '../../../../components';
+import { dummy } from '../../../../assets/documents';
 import { Helmet } from 'react-helmet';
 import './styles.css';
 
@@ -11,10 +12,14 @@ export default () => (
     <VerticalNavbar/>
     <Layout>
       <div className='stab52f20-div'>
-        <h3>September 17, 2020</h3>
+        <h3>Tutorial Information</h3>
+        <h4><b>Meeting Time</b>: Thursdays @ 17:00-18:00</h4>
+        <h4><b>Meeting Location</b>: Blackboard Collaborate</h4>
+        <h3>Tutorial Notes</h3>
+        <h5>September 17, 2020</h5>
         <ul>
           <li>
-            <a href='/teaching/stab52f20'>STAB53</a>
+            <a href={dummy}>Probability Models & Combinatorics</a>
           </li>
         </ul>
       </div>
