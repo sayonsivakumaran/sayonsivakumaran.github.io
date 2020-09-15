4ab922af810e4f8f5932b48844c5fdab74684826
Sayon Sivakumaran <sivakumaransayon@gmail.com>
2020-09-15 13:56:41 -0400

    Update the About section

--- src/routes/About/index.js
+++ src/routes/About/index.js
@@ -12,40 +12,15 @@ export default () => (
     <VerticalNavbar page='About'/>
     <Layout>
       <div className="modal-body row">
-        <div className="col-md-6 about-div">
-          <BioImage img={aboutImage}/>
-        </div>
-        <div className="col-md-6 about-div">
+        <div className="about-div">
           <h1>About Me</h1>
-          <h4>Hello!</h4>
-          <h4>
-            I'm <b>Sayon Sivakumaran</b>, a second year Computer Science and Statistics student at the University of Toronto, Scarborough campus.
-            Recently, I completed a co-op placement at Leonardo Worldwide as a full-stack software developer, where I worked on web applications using various different front-end and back-end technologies.
-            Some of my technology interests include virtual reality, artificial intelligence, and cloud computing.
-          </h4>
-          <h4>
-            <b>Projects</b>
-            <ul>
-              <li><a href='https://github.com/sayonsivakumaran/JShell'>JShell</a></li>
-              <li><a href='https://github.com/sayonsivakumaran/JetFighters'>Jet Fighters</a></li>
-              <li><a href='https://github.com/sayonsivakumaran/SudokuAndroid'>Sudoku</a></li>
-              <li><a href='https://github.com/sayonsivakumaran/Bingo'>Bingo</a></li>
-              <li><a href='https://github.com/sayonsivakumaran/ResearchProject'>Statistical Research Project</a></li>
-            </ul>
-          </h4>
-          <h4>
-            <b>Tools & Technologies</b>
-            <ul>
-              <li>JavaScript</li>
-              <li>Java</li>
-              <li>C</li>
-              <li>HTML</li>
-              <li>React.js</li>
-              <li>Marko.js (SPA JavaScript Framework, similar to React.js)</li>
-              <li>Python</li>
-              <li>CSS</li>
-            </ul>
-          </h4>
+          <p>
+            I'm <b>Sayon Sivakumaran</b>, a third year Computer Science and Statistics student at the <b>University of Toronto</b>,
+            where I am working as a Teaching Assistant for the Introductory Probability course STAB52.
+            I am currently working as a <b>Data Engineer</b>, and have prior experience working as a <b>Full Stack Engineer</b>.
+            When I'm not working or studying, I spend most of my time surfing the internet, and watching or reading about movies and television.
+            Thanks for visiting my site! Feel free to check out some of the projects I've worked on on <a href='https://github.com/sayonsivakumaran'>GitHub</a>, and don't hesistate to reach out!
+          </p>
         </div>
       </div>
     </Layout>
