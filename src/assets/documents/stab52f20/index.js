b7a24f4424522570d8e60b4d366c9ae2cd04cc61
Sayon Sivakumaran <sivakumaransayon@gmail.com>
2020-11-05 18:10:13 -0500

    Add PDF for week 8 to document list

--- src/assets/documents/stab52f20/index.js
+++ src/assets/documents/stab52f20/index.js
@@ -5,3 +5,4 @@ export { default as stab52f20w3 } from './document-week3.pdf';
 export { default as stab52f20w4 } from './document-week4.pdf';
 export { default as stab52f20w6 } from './document-week6.pdf';
 export { default as stab52f20w7 } from './document-week7.pdf';
+export { default as stab52f20w8 } from './document-week8.pdf';
